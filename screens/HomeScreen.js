2a2fab5146ac236867ad4506655c7e82760717f5
NicolasHasbun <Ni.Hasbun.Molina@gmail.com>
2023-12-09 02:25:25 -0300

    screens,styles

---
+++ screens/HomeScreen.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { COLORS } from "../constants/Color";
+
+const HomeScreen = () => {
+
+    const navigation = useNavigation();
+
+    return (
+        <View>
+        <Text
+            style={{
+                fontSize: 30,
+                textAlign: "center",
+                marginTop: "20%"
+            }}
+            >Home Screen</Text>
+            <TouchableOpacity
+            onPress={() => navigation.navigate("Stack")}
+                style={{
+                    backgroundColor: COLORS.primary,
+                    padding: 10,
+                    marginTop: "20%",
+                    width: "50%",
+                    alignSelf : "center",
+                    borderRadius: 10,
+                }}
+            >
+            <Text
+                style={{
+                    fontSize: 15,
+                    textAlign: "center",
+                    color: COLORS.white,
+                    fontFamily: 'Roboto-Bold'
+                }}
+            >Ir a Stack Screen</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+export default HomeScreen;
